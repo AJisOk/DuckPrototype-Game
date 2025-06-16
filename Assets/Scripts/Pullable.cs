39f03072b6d631e1c2badbc5a9d59917cf2dc1aa
AJ Roberts/Home <ajroberts2751@gmail.com>
2025-06-16 12:19:34 -0700

    Added feedback when the player grabs a pullable object

--- Assets/Scripts/Pullable.cs
+++ Assets/Scripts/Pullable.cs
@@ -4,14 +4,29 @@ public class Pullable : MonoBehaviour
 {
     [SerializeField] protected FixedJoint _fixedJoint;
     [SerializeField] protected int _duckLayer = 7;
+    [SerializeField] protected Material _grabbedMaterial;
 
+    private Material _defaultMaterial;
     private bool _isTargeted = false;
+    private MeshRenderer _meshRenderer;
 
     public bool IsTargeted {get => _isTargeted; private set => _isTargeted = value; }
+    public bool IsGrabbed { get => (_fixedJoint.connectedBody != null); }
 
     private void Awake()
     {
         if(_fixedJoint == null) _fixedJoint = GetComponent<FixedJoint>();
+        if (_meshRenderer == null)
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _defaultMaterial = _meshRenderer.material;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (IsGrabbed && _meshRenderer.material == _defaultMaterial) _meshRenderer.material = _grabbedMaterial;
+        //if (!IsGrabbed && _meshRenderer.material == _grabbedMaterial) _meshRenderer.material = _defaultMaterial;
     }
 
     public void OnTargeted()
@@ -23,8 +38,8 @@ public class Pullable : MonoBehaviour
 
     public void OnUntargeted()
     {
+        if (IsGrabbed) _meshRenderer.material = _defaultMaterial;
         _isTargeted = false;
-
         _fixedJoint.connectedBody = null;
     }
 
@@ -35,8 +50,8 @@ public class Pullable : MonoBehaviour
 
         print("Duck Collided with Pullable");
 
-        
 
+        _meshRenderer.material = _grabbedMaterial;
         _fixedJoint.connectedBody = (Rigidbody)collision.body;
     }
 }
