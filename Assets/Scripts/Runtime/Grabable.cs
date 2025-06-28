e52963f20c224ebe8721cec2bb45a8c862ab7b64
AJ Roberts <ajroberts2751@gmail.com>
2025-06-27 20:35:13 -0700

    new pullable popup text shows required ducklings now, does not show how many player has

--- Assets/Scripts/Runtime/Grabable.cs
+++ Assets/Scripts/Runtime/Grabable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
--- Assets/Scripts/Runtime/NewPullable.cs
+++ Assets/Scripts/Runtime/NewPullable.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework.Constraints;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +9,7 @@ public class NewPullable : Grabable
     [Header("Pullable")]
     [SerializeField] protected FixedJoint _fixedJoint;
     [SerializeField, Range(0, 5)] protected int _ducklingsRequiredToPull;
+    [SerializeField] protected TextMeshProUGUI _ducklingsRequiredText;
     [SerializeField] float _pullingSpeed = 2f;
     [SerializeField] float _acceleration = 1f;
 
@@ -16,6 +18,13 @@ public class NewPullable : Grabable
     private bool _isBeingPulled = false;
 
     public bool IsBeingPulled { get => _isBeingPulled; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if(_ducklingsRequiredText != null) _ducklingsRequiredText.text = "0/" + _ducklingsRequiredToPull.ToString();
+    }
 
     protected override void OnTriggerEnter(Collider other)
     {
