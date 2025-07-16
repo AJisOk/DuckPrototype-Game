6baefc84bfff677b1b7eb991d5729554803a8eb0
AJ Roberts <ajroberts2751@gmail.com>
2025-07-16 16:36:25 -0700

    Created bobbing script, applied it to some sprites in main menu scene. Further decorated and populated main menu scene

--- Assets/Scripts/Runtime/Art/SpriteBuoyancy.cs
+++ Assets/Scripts/Runtime/Art/SpriteBuoyancy.cs
@@ -10,11 +10,19 @@ public class SpriteBuoyancy : MonoBehaviour
     [SerializeField] private SpriteRenderer _sprite;
 
     [Header("Motion")]
-    [SerializeField] private float _interval = 1f;
-    [SerializeField] private float _offset = 1f;
+    [SerializeField] private float _interval = 2f;
+    [SerializeField] private float _offsetDistance = .3f;
     [SerializeField] private AnimationCurve _motionCurve;
 
     private float timer = 0f;
+    private float initialYPosition;
+    
+
+    private void Awake()
+    {
+        if(_sprite == null) _sprite = GetComponent<SpriteRenderer>();
+        initialYPosition = transform.position.y;
+    }
 
     private void Start()
     {
@@ -23,13 +31,28 @@ public class SpriteBuoyancy : MonoBehaviour
 
     private IEnumerator BuoyancyCoroutine()
     {
-        
-        
-        //move up
+        //offset start time so sprites with same interval don't fully mirror eachother
 
-        //move back down
+        timer = Random.Range(0f, _interval);
+        Vector3 offset;
+        float yClampedPosition;
+        float yTargetPosition;
+        float yOffset;
+        while (true)
+        {
+            yClampedPosition = _motionCurve.Evaluate(Mathf.InverseLerp(0f, _interval, timer));
+            yTargetPosition = yClampedPosition * _offsetDistance + initialYPosition;
+            yOffset = yTargetPosition - _sprite.transform.position.y;
+            offset = new Vector3(0f, yOffset, 0f);
 
+            _sprite.transform.Translate(offset);
 
+            timer += Time.deltaTime;
+            if (timer >= _interval) timer = 0f;
+
+            
+            yield return null;
+        }
 
         yield return null;
     }
