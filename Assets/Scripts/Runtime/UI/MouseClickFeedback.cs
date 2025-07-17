3a2f1cc00181376d223a5c85c5d675c069f79c1f
AJ Roberts <ajroberts2751@gmail.com>
2025-07-17 16:37:30 -0700

    Thud sound now plays when pullables is clicked

--- Assets/Scripts/Runtime/UI/MouseClickFeedback.cs
+++ Assets/Scripts/Runtime/UI/MouseClickFeedback.cs
@@ -5,20 +5,31 @@ using FMODUnity;
 public class MouseClickFeedback : MonoBehaviour
 {
     [SerializeField] protected GameObject _prefabToInstantiate;
-    [SerializeField] protected LayerMask _layerMask;
-    [SerializeField] protected EventReference _clickSound;
+    [SerializeField] protected LayerMask _waterLayerMask;
+    [SerializeField] protected LayerMask _pullableLayerMask;
+    [SerializeField] protected EventReference _waterClickSound;
+    [SerializeField] protected EventReference _pullableClickSound;
     [SerializeField] protected Vector3 _instantiateOffset;
     public void OnMoveTo(InputValue value)
     {
         RaycastHit hit;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit,Mathf.Infinity, _layerMask, QueryTriggerInteraction.Ignore ))
+
+        if (Physics.Raycast(ray, out hit,Mathf.Infinity, _pullableLayerMask, QueryTriggerInteraction.Ignore))
+        {
+            //play pullable click sound
+            RuntimeManager.PlayOneShot(_pullableClickSound);
+
+            return;
+        }
+
+        if (Physics.Raycast(ray, out hit,Mathf.Infinity, _waterLayerMask, QueryTriggerInteraction.Ignore ))
         {
             Instantiate(_prefabToInstantiate, hit.point + _instantiateOffset, Quaternion.identity);
 
             //play water click sound
-            RuntimeManager.PlayOneShot(_clickSound);
+            RuntimeManager.PlayOneShot(_waterClickSound);
         }
     }
 }
