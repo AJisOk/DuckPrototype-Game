2511e282a3d732443bd606dcbb1d20b54a36fcf3
AJ Roberts <ajroberts2751@gmail.com>
2025-07-18 11:59:36 -0700

    Updated pause menu canvas to include ui elements that appear when the player completes all objectives, created game manager script that tracks npcs helped and duckligns collected, updated pause menu icon

---
+++ Assets/Scripts/Runtime/GameSystems/GameManager.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //game manager needs to track ducklings collected and npc objects delivered
+
+
+    [SerializeField] private int _ducklingsTotal = 0;
+    [SerializeField] private int _nPCsTotal = 0;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI _ducklingText;
+    [SerializeField] private TextMeshProUGUI _nPCText;
+    [SerializeField] private CanvasGroup _ducklingsCompleteCG;
+    [SerializeField] private CanvasGroup _nPCCompleteCG;
+
+    private int ducklingsCollected = 0;
+    private int nPCsHelped = 0;
+
+    private bool allNPCsHelped = false;
+    private bool allDucklingsCollected = false;
+
+    public void NPCHelped()
+    {
+        if (allNPCsHelped) return;
+
+        nPCsHelped++;
+        _nPCText.text = "Beavers Helped: " + nPCsHelped.ToString() + " / " + _nPCsTotal.ToString();
+
+        if (nPCsHelped == _nPCsTotal)
+        {
+            allNPCsHelped = true;
+
+            _nPCCompleteCG.alpha = 1;
+        }
+    }
+
+    public void DucklingCollected()
+    {
+        if (allDucklingsCollected) return;
+
+        ducklingsCollected++;
+        _nPCText.text = "Ducklings Found: " + ducklingsCollected.ToString() + " / " + _ducklingsTotal.ToString();
+
+        if (ducklingsCollected == _ducklingsTotal)
+        {
+            allDucklingsCollected = true;
+
+            _ducklingsCompleteCG.alpha = 1;
+        }
+    }
+}
